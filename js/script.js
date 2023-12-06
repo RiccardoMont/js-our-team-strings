f169e7d2d10b15400a16568a4c389ecac53d20b8
RiccardoMont <riccardomontalto94@gmail.com>
2023-12-06 11:20:44 +0100

    Milestone 0-1 fatte. Working on n°3

--- js/script.js
+++ js/script.js
@@ -1,1 +1,40 @@
 console.log('si può fare!');
+
+
+const teamMembers = ['Pippo', 'Pluto', 'Topolino', 'Paperino', 'Qui', 'Quo', 'Qua', 'Paperoga', 'Gastone'];
+const container = document.querySelector('.container');
+
+
+
+
+for(let i = 0; i < teamMembers.length; i++){
+
+    const member = teamMembers[i];
+
+    //METODO INSERTADJACENT
+    //const cardMember = `<div class="card d-flex justify-content-center align-items-center">${member}</div>`;
+    //container.insertAdjacentHTML('beforeend', cardMember);
+    //console.log(cardMember);
+
+    //METODO CREATE-APPEND
+    const div = document.createElement('div');
+    div.classList.add('card', 'd-flex', 'justify-content-center', 'align-items-center');
+    div.append(`${member}`);
+    container.append(div);
+
+
+
+    console.log(teamMembers[i]);
+
+}
+
+
+const card = document.querySelector('.card');
+
+
+
+card.addEventListener('click', function () {
+
+    console.log('hai cliccato');
+
+})
