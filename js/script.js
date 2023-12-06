9203afdac6f644cccd0edbb42edd6cf29f60b6cf
RiccardoMont <riccardomontalto94@gmail.com>
2023-12-06 23:16:11 +0100

    working on bonus. Inserite immagini

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ console.log('si può fare!');
 
 
 const teamMembers = ['Pippo', 'Pluto', 'Topolino', 'Paperino', 'Qui', 'Quo', 'Qua', 'Paperoga', 'Gastone'];
+
+const imgMembers = ['https://upload.wikimedia.org/wikipedia/en/5/50/Goofy_Duckipedia.png', 'https://upload.wikimedia.org/wikipedia/en/b/b2/Pluto_%28Disney%29_transparent.png', 'https://upload.wikimedia.org/wikipedia/en/d/d4/Mickey_Mouse.png', 'https://it.wikifur.com/w/images/thumb/6/6f/Donald_Duck.png/362px-Donald_Duck.png', 'https://www.topolino.it/wp-content/uploads/2019/12/quiquoequaintera.png', 'https://www.topolino.it/wp-content/uploads/2019/12/quiquoequaintera.png', 'https://www.topolino.it/wp-content/uploads/2019/12/quiquoequaintera.png' ,'https://www.topolino.it/wp-content/uploads/2019/12/paperoga_intera.png', 'https://upload.wikimedia.org/wikipedia/en/2/22/GladstoneGander.jpg'];
+
 const container = document.querySelector('.container');
 
 
@@ -10,6 +13,7 @@ const container = document.querySelector('.container');
 for(let i = 0; i < teamMembers.length; i++){
 
     const member = teamMembers[i];
+    const avatar = imgMembers[i];
 
     //METODO INSERTADJACENT
     //const cardMember = `<div class="card d-flex justify-content-center align-items-center">${member}</div>`;
@@ -18,8 +22,24 @@ for(let i = 0; i < teamMembers.length; i++){
 
     //METODO CREATE-APPEND
     const div = document.createElement('div');
-    div.classList.add('card', 'd-flex', 'justify-content-center', 'align-items-center');
-    div.append(`${member}`);
+    const h3 = document.createElement('h3');
+    const img = document.createElement('img');
+    const p = document.createElement('p');
+    //img.src = "https://picsum.photos/300/200";
+    div.style.width = "30%";
+    img.style.height = "200px";
+    img.src = `${avatar}`;
+
+
+    div.classList.add('card', 'd-flex', 'justify-content-center', 'align-items-center', 'p-4', 'mb-4');
+    //img.classList.add('h-200px');
+
+    div.append(img);
+    div.append(h3);
+    div.append(p);
+    p.append(`Lorem ipsum dolor`)
+    h3.append(`${member}`);
+    
     container.append(div);
 
 
@@ -29,12 +49,29 @@ for(let i = 0; i < teamMembers.length; i++){
 }
 
 
-const card = document.querySelector('.card');
+const card = document.querySelectorAll('.card');
+
+for(let i = 0; i < card.length; i++){
+    
+    card[i].addEventListener('click', function () {
+
+        //const div = document.querySelector('.card');
+        console.log('hai cliccato');
 
 
 
-card.addEventListener('click', function () {
+        if(card[i].classList.contains('bg-primary')){
+            console.log('dentro if');
+            card[i].classList.remove('bg-primary');
 
-    console.log('hai cliccato');
+        } else {
+            console.log('dentro else');
+            card[i].classList.add('bg-primary');
 
-})
+        }
+        
+    })
+}
+
+
+
