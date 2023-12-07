ede6150b429b702df95eddbbd419e8ffbb78bf33
RiccardoMont <riccardomontalto94@gmail.com>
2023-12-07 11:15:25 +0100

    aggiunto form per inserire testo ed immagine

--- js/script.js
+++ js/script.js
@@ -5,7 +5,8 @@ const teamMembers = ['Pippo', 'Pluto', 'Topolino', 'Paperino', 'Qui', 'Quo', 'Qu
 
 const imgMembers = ['https://upload.wikimedia.org/wikipedia/en/5/50/Goofy_Duckipedia.png', 'https://upload.wikimedia.org/wikipedia/en/b/b2/Pluto_%28Disney%29_transparent.png', 'https://upload.wikimedia.org/wikipedia/en/d/d4/Mickey_Mouse.png', 'https://it.wikifur.com/w/images/thumb/6/6f/Donald_Duck.png/362px-Donald_Duck.png', 'https://www.topolino.it/wp-content/uploads/2019/12/quiquoequaintera.png', 'https://www.topolino.it/wp-content/uploads/2019/12/quiquoequaintera.png', 'https://www.topolino.it/wp-content/uploads/2019/12/quiquoequaintera.png' ,'https://www.topolino.it/wp-content/uploads/2019/12/paperoga_intera.png', 'https://upload.wikimedia.org/wikipedia/en/2/22/GladstoneGander.jpg'];
 
-const container = document.querySelector('.container');
+const row = document.querySelector('.row');
+const aggiungi = document.getElementById('aggiungi');
 
 
 
@@ -27,13 +28,14 @@ for(let i = 0; i < teamMembers.length; i++){
     const p = document.createElement('p');
     //img.src = "https://picsum.photos/300/200";
     div.style.width = "30%";
+    // Non mi adotta le classi se le aggiungo con classlist ed ho quindi scelto questo metodo
     img.style.height = "200px";
     img.style.opacity = "0.5";
     img.src = `${avatar}`;
 
 
     div.classList.add('card', 'd-flex', 'justify-content-center', 'align-items-center', 'gap-2', 'p-4', 'mb-4', 'bg-warning-subtle');
-    //img.classList.add('h-200px');
+    
 
     div.append(img);
     div.append(h3);
@@ -41,13 +43,17 @@ for(let i = 0; i < teamMembers.length; i++){
     p.append(`Lorem ipsum dolor`)
     h3.append(`${member}`);
     
-    container.append(div);
+    row.append(div);
 
 
 
     console.log(teamMembers[i]);
 
 }
+
+
+
+
 
 
 const card = document.querySelectorAll('.card');
@@ -57,9 +63,8 @@ for(let i = 0; i < card.length; i++){
     
     card[i].addEventListener('click', function () {
 
-        //const div = document.querySelector('.card');
         console.log('hai cliccato');
-        
+        console.log(card.length);
 
 
         if(card[i].classList.contains('bg-warning-subtle')){
@@ -76,6 +81,55 @@ for(let i = 0; i < card.length; i++){
         
     })
 }
+
+
+aggiungi.addEventListener('click', function () {
+
+    const newMember = document.getElementById('newMember').value;
+    const newImg = document.getElementById('newImg').value;
+
+    
+    console.log(newMember);
+    console.log(newImg);
+
+    if (newMember == '' || newImg == ''){
+
+        console.log('I campi non sono stati riempiti correttamente');
+
+    } else {
+
+        teamMembers.push(newMember);
+        imgMembers.push(newImg);
+        console.log('sei in else-add');
+        console.log(teamMembers);
+        console.log(imgMembers);
+        console.log(card.length);
+
+        const div = document.createElement('div');
+        const h3 = document.createElement('h3');
+        const img = document.createElement('img');
+        const p = document.createElement('p');
+
+        div.style.width = "30%";
+        img.style.height = "200px";
+        img.style.opacity = "0.5";
+        img.src = `${newImg}`;
+
+        div.classList.add('card', 'd-flex', 'justify-content-center', 'align-items-center', 'gap-2', 'p-4', 'mb-4', 'bg-warning-subtle');
+
+        div.append(img);
+        div.append(h3);
+        div.append(p);
+        p.append(`Lorem ipsum dolor`)
+        h3.append(`${newMember}`);
+    
+        row.append(div);
+
+
+
+    }
+
+})
 
 
 
